ba7a1db3879732f2219708cd74b612f4c2268ba8
mdkeydama23 <mdk@keydama.com>
2024-02-08 00:17:53 +0300

    Update database URI to JAWSDB_MARIA_URL

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') 
-    SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI =  os.environ.get('JAWSDB_MARIA_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask-Login settings
